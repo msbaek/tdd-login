856b6abcb618859f0a249509ecc6c26fa3289b71
Myeongseok Baek <msbaek@daumcorp.com>
2013-09-25 15:46:06 +0900

    invalid id or pwd - add failing test

--- src/test/java/AuthenticateTest.java
+++ src/test/java/AuthenticateTest.java
@@ -1,2 +1,12 @@
+import org.junit.Test;
+
 public class AuthenticateTest {
+    @Test
+    public void whenIdOrPwdIsInvalid_thenThrowException() {
+        try {
+            authenticate(null, null);
+            fail("InvalidIdOrPwd expected");
+        } catch (InvalidIdOrPwd e) {
+        }
+    }
 }
